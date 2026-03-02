a2864188587c5b0e8f6eac028150a6971f7f2d8c
Cooper Brown <cooperbrown.dev@gmail.com>
2026-03-02 15:32:47 -0600

    Removed notes

--- Stage 2/Week 3/s2w3Challenge/Program.cs
+++ Stage 2/Week 3/s2w3Challenge/Program.cs
@@ -7,12 +7,6 @@
             IGrader grader = new UndergradStudentGrader();
             IGrader grader2 = new PostgradStudentGrader();
 
-            //double grade1 = 85;
-            //double grade2 = 87;
-            //double grade3 = 94;
-
-            //ValidateGrades(grade1, grade2, grade3);
-
             GradingService gradingService = new GradingService(grader);
             gradingService.CalculateGrade(85, 87, 94);
 
@@ -20,17 +14,5 @@
             gradingService2.CalculateGrade(91, 77, 88);
         }
 
-        //public double ValidateGrades(double grade1, double grade2, double grade3)
-        //{
-        //    Console.WriteLine("Inside Grade Validator.");
-        //    // validate grades are between 0 and 100
-        //    if (grade1 < 0 || grade1 > 100 ||
-        //        grade2 < 0 || grade2 > 100 ||
-        //        grade3 < 0 || grade3 > 100)
-        //    {
-        //        throw new ArgumentOutOfRangeException("Grades entered must be between 0 and 100.");
-        //    }
-        //    return (grade1, grade2, grade3);
-        //}
     }
 }
