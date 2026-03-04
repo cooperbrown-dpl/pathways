f9e90823cf55ba3f6bef6fc8daa2289870dbab58
Cooper Brown <cooperbrown.dev@gmail.com>
2026-03-04 10:01:37 -0600

    Created NFL Team CRUD

---
+++ Stage 2/Week 4/TodoApi/TodoApi/wwwroot/js/nfl.js
@@ -0,0 +1,142 @@
+﻿const uri = 'api/nflteams';
+let nflTeams = [];
+
+function getItems() {
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+        .catch(error => console.error('Unable to get items.', error));
+}
+
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+    const addWinsTextbox = document.getElementById('add-wins');
+    const addLossesTextbox = document.getElementById('add-losses');
+    const addQuarterbackTextbox = document.getElementById('add-quarterback');
+
+    const item = {
+        name: addNameTextbox.value.trim(),
+        wins: addWinsTextbox.value.trim(),
+        losses: addLossesTextbox.value.trim(),
+        quarterback: addQuarterbackTextbox.value.trim(),
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+            addWinsTextbox.value = '';
+            addLossesTextbox.value = '';
+            addQuarterbackTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
+
+function deleteItem(id) {
+    fetch(`${uri}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to delete item.', error));
+}
+
+function displayEditForm(id) {
+    const item = nflTeams.find(item => item.id === id);
+
+    document.getElementById('edit-name').value = item.name;
+    document.getElementById('edit-wins').value = item.wins;
+    document.getElementById('edit-losses').value = item.losses;
+    document.getElementById('edit-quarterback').value = item.quarterback;
+    document.getElementById('edit-id').value = item.id;
+    document.getElementById('editForm').style.display = 'block';
+}
+
+function updateItem() {
+    const itemId = document.getElementById('edit-id').value;
+    const item = {
+        id: parseInt(itemId, 10),
+        name: document.getElementById('edit-name').value.trim(),
+        wins: document.getElementById('edit-wins').value.trim(),
+        losses: document.getElementById('edit-losses').value.trim(),
+        quarterback: document.getElementById('edit-quarterback').value.trim()
+
+    };
+
+    fetch(`${uri}/${itemId}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(() => getItems())
+        .catch(error => console.error('Unable to update item.', error));
+
+    closeInput();
+
+    return false;
+}
+
+function closeInput() {
+    document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+    const name = (itemCount === 1) ? 'NFL Team' : 'NFL Teams';
+
+    document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+    const tBody = document.getElementById('nflTeams');
+    tBody.innerHTML = '';
+
+    _displayCount(data.length);
+
+    const button = document.createElement('button');
+
+    data.forEach(item => {
+        let editButton = button.cloneNode(false);
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+
+        let deleteButton = button.cloneNode(false);
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        let textNode = document.createTextNode(item.name);
+        td1.appendChild(textNode);
+
+        let td2 = tr.insertCell(1);
+        let textNode2 = document.createTextNode(item.wins);
+        td2.appendChild(textNode2);
+
+        let td3 = tr.insertCell(2);
+        let textNode3 = document.createTextNode(item.losses);
+        td3.appendChild(textNode3);
+
+        let td4 = tr.insertCell(3);
+        let textNode4 = document.createTextNode(item.quarterback);
+        td4.appendChild(textNode4);
+
+        let td5 = tr.insertCell(4);
+        td5.appendChild(editButton);
+
+        let td6 = tr.insertCell(5);
+        td6.appendChild(deleteButton);
+    });
+
+    nflTeams = data;
+}
