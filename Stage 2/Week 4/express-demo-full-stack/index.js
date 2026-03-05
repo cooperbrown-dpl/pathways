81ab590a56da2a66877b351ab42a9a4d9860c201
Cooper Brown <cooperbrown.dev@gmail.com>
2026-03-05 17:01:49 -0600

    first commit

--- Stage 2/Week 4/express-demo-full-stack/index.js
+++ Stage 2/Week 4/express-demo-full-stack/index.js
@@ -4,22 +4,46 @@ const app = express();
 
 app.use(express.json());
 
-const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' }
+const songs = [
+    {
+        id: 1,
+        title: "In the End",
+        artist: "Linkin Park",
+        album: "Hybrid Theory",
+        releaseYear: 2000,
+        genre: "Nu-Metal",
+        isExplicit: false
+    },
+    {
+        id: 2,
+        title: "Worldwide choppers",
+        artist: "Tech N9ne",
+        album: "All 6's and 7's",
+        releaseYear: 2011,
+        genre: "Hip-Hop",
+        isExplicit: true
+    },
+    {
+        id: 3,
+        title: "Down",
+        artist: "311",
+        album: "311",
+        releaseYear: 1995,
+        genre: "Alternative Rock",
+        isExplicit: false
+    }
 ];
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello Full Stack');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send(courses);
+    res.send(songs);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
+    const course = songs.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('The course with the given ID was not found.');
     res.send(course);
 });
@@ -29,15 +53,15 @@ app.post('/api/courses', (req, res) =>{
     if (error) return res.status(400).send(error.details[0].message);
     
     const course = {
-        id: courses.length + 1,
+        id: songs.length + 1,
         name: req.body.name
     };
-    courses.push(course);
+    songs.push(course);
     res.send(course);
 })
 
 app.put('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
+    const course = songs.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('The course with the given ID was not found.');
 
     const { error } = validateCourse(req.body); // result.error
@@ -48,17 +72,17 @@ app.put('/api/courses/:id', (req, res) => {
 })
 
 app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
+    const course = songs.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('The course with the given ID was not found.');
 
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
+    const index = songs.indexOf(course);
+    songs.splice(index, 1);
 
     res.send(course);
 })
 
 // PORT
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
