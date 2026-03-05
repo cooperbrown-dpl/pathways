3649cee2ad3df5c811f208b64a63bd5c22df3ffd
Cooper Brown <cooperbrown.dev@gmail.com>
2026-03-05 15:27:19 -0600

    Added AsNoTracking() on get to save resources

--- Stage 2/Week 4/TodoApi/TodoApi/Controllers/NFLTeamsController.cs
+++ Stage 2/Week 4/TodoApi/TodoApi/Controllers/NFLTeamsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
@@ -24,7 +19,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<NFLTeam>>> GetNFLTeams()
         {
-            return await _context.NFLTeams.ToListAsync();
+            return await _context.NFLTeams.AsNoTracking().ToListAsync();
         }
 
         // GET: api/NFLTeams/5
