91ee1ccbca4710a1a7907b9355676d365c3a5f2c
Cooper Brown <cooperbrown.dev@gmail.com>
2026-03-10 10:22:14 -0500

    added nullable strings

--- Stage 2/Week 5/TVShowWebAPIw5Challenge/TVShowWebAPIw5Challenge/Models/TVShow.cs
+++ Stage 2/Week 5/TVShowWebAPIw5Challenge/TVShowWebAPIw5Challenge/Models/TVShow.cs
@@ -5,7 +5,7 @@ public class TVShow
     public long Id { get; set; }
     public string? Title { get; set; }
     public int TotalSeasons { get; set; }
-    public string Platform { get; set; }
-    public string TVRating { get; set; }
+    public string? Platform { get; set; }
+    public string? TVRating { get; set; }
     public bool HaveStartedWatching { get; set; }
 }
