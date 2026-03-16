1832a743557d4cddec5c54d6646e166ad0f940c5
cooperbrown-dev <cooperbrown.dev@gmail.com>
2026-03-16 12:57:20 -0500

    first commit

---
+++ Stage 3/Week 1/Day1/Program.cs
@@ -0,0 +1,54 @@
+﻿// 1. Build out to support add/sub/mul/div
+// 2. Support operations in lower and upper case
+// 3. Convert do..while to a while loop.
+
+var yesNo = "yes";
+while(yesNo == "yes")
+{
+  Console.WriteLine("Enter number: ");
+  var number1 = Console.ReadLine();
+  var num1 = validateNumber(number1);
+
+  Console.WriteLine("Enter number: ");
+  var number2 = Console.ReadLine();
+  var num2 = validateNumber(number2);
+
+  Console.WriteLine("Enter Operation (add, sub, mul, div): ");
+  var operation = Console.ReadLine().ToLower();
+
+  switch (operation)
+  {
+    case "add":
+      Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+      break;
+    case "sub":
+      Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+      break;
+    case "mul":
+      Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+      break;
+    case "div":
+      Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+      break;
+    default:
+      Console.WriteLine("Invalid operation");
+      break;
+  }
+
+  Console.WriteLine("Continue (yes/no)");
+  yesNo = Console.ReadLine().ToLower();
+}
+
+Console.WriteLine("Exiting program, goodbye!");
+
+double validateNumber(string input){
+    if (double.TryParse(input, out double number))
+    {
+        return number;
+    }
+    else
+    {
+        Console.WriteLine("Invalid number, please try again.");
+        return validateNumber(Console.ReadLine());
+    }
+};
---
+++ Stage 3/Week 1/Day1/figmaFundamentalsCatchup/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+List<int> numList = new List<int>();
+
+numList.Add(8);
+numList.Add(2);
+numList.Add(3);
+numList.Add(7);
+numList.Add(9);
+
+
+
+foreach(int num in numList)
+{
+    Console.WriteLine(num);
+}
+
+Console.WriteLine(numList.Average());
+
+numList.Sort();
+foreach(int num in numList)
+{
+    Console.WriteLine(num);
+}
