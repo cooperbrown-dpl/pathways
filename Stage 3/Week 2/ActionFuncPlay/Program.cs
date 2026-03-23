23b8aa19b08682a926b24fb6e25c94ecd9ebe650
Cooper Brown <cooperbrown.dev@gmail.com>
2026-03-23 12:02:00 -0500

    first commit

---
+++ Stage 3/Week 2/ActionFuncPlay/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Actions & Funcs playground\n");
+
+        SayHello("Valerie");
+
+        Action<string> sayHello = name => Console.WriteLine($"Hello {name}!");
+
+        sayHello("Cooper");
+
+        Action<int> showNumber = n => Console.WriteLine($"The number is {n}");
+
+        showNumber(7);
+        showNumber(42);
+        showNumber(-3);
+
+        Func<int, int, int> add = (a, b) => a + b;
+        Func<int, int, int> multiply = (a, b) => a * b;
+
+        Console.WriteLine(add(5, 6));
+        Console.WriteLine(multiply(4, 9));
+
+        DoTwice(() => Console.WriteLine("Hi!"));
+        DoTwice(() => Console.WriteLine("Click!"));
+
+        Console.WriteLine(Calculate(10, 3, (x, y) => x + y));
+        Console.WriteLine(Calculate(11, 3, (x, y) => x / y));
+
+        Console.WriteLine("\nPress ENTER to exit...");
+        Console.ReadLine();
+    }
+
+
+    static int Calculate(int a, int b, Func<int, int, int> operation)
+    {
+        return operation(a, b);
+    }
+
+    static void DoTwice(Action action)
+    {
+        action();
+        action();
+    }
+
+
+    static void SayHello(string name)
+    {
+        Console.WriteLine($"Hello {name}!");
+    }
+
+    // You can add your own small helper methods here later
+}
---
+++ Stage 3/Week 2/lambda2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+Console.WriteLine("Lambda playground started\n");
+
+Func<int, int> doubleNum = n => n * 2;
+
+Console.WriteLine(doubleNum(3));
+
+Action<string> sayNameIsAwesome = name => Console.WriteLine($"You're awesome, {name}!");
+
+sayNameIsAwesome("Valerie");
+sayNameIsAwesome("Jim");
+sayNameIsAwesome("Dominic");
+
+Func<int, bool> isItEven = n => n % 2 == 0;
+
+Console.WriteLine($"4: {isItEven(4)}");
+Console.WriteLine($"7: {isItEven(7)}");
+Console.WriteLine($"0: {isItEven(0)}");
+Console.WriteLine($"13: {isItEven(13)}");
+
+Twice(() => Console.WriteLine("Hello Twice"));
+
+Console.WriteLine(Operate(5, 5, (a, b) => a * b));
+
+Func<string> greetingBasedOnHour = () =>
+{
+    int h = DateTime.Now.Hour;
+    return h < 12 ? "Morning!" : h < 17 ? "Afternoon!" : "Evening!";
+};
+
+Console.WriteLine($"Good {greetingBasedOnHour()}");
+
+Func<int, int> numSquared = n => n * n;
+
+Console.WriteLine(numSquared(4));
+
+Console.WriteLine("\nDone. Press ENTER to close...");
+Console.ReadLine();
+
+int Operate(int a, int b, Func<int, int, int> op)
+{
+    return op(a, b);
+}
+
+void Twice(Action action)
+{
+    action();
+    action();
+}
