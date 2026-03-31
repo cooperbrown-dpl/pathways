962947d0bd78399e760afcf7e76ad5c3b4d1623e
Cooper Brown <cooperbrown.dev@gmail.com>
2026-03-31 13:23:19 -0500

    first commit

---
+++ Stage 3/Week 3/Scratch/Program.cs
@@ -0,0 +1,12 @@
+﻿void Swap<T>(ref T a, ref T b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+int x = 1, y = 2;
+Swap(ref x, ref y);
+Console.WriteLine($"{x}, {y}"); // 2, 1
+string s1 = "hello", s2 = "world";
+Swap(ref s1, ref s2);
+Console.WriteLine($"{s1}, {s2}"); // world, hello
